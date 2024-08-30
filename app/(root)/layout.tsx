7c975e8b8c8d3afec0851e8cb2b841307938f3f9
sadiq1aziz <sadiq1aziz@gmail.com>
2024-08-30 19:08:25 +0400

    feat(feature_1.0): Implement MobileNavBar with Hamburger trigger - added new components for MobileNav.tsx - added new css to global + ui components from shadercn
    This feature is part of the initial rollout for the user dashboard.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,7 @@
 import SideBar from "@/components/ui/SideBar";
 import React from "react";
+import Image from "next/image";
+import MobileNav from "@/components/ui/MobileNav";
 
 // here we use tsx to define the children props being read only
 // and also being renderable types such as JSX, frags, etc
@@ -12,13 +14,29 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   //create dummy user
-  const localUser = {firstName:'Aashiq', lastName:'Aziz'};
+  const localUser = { firstName: "Aashiq", lastName: "Aziz" };
 
   return (
-    /* We add some custom style to make the sidebar appear on the left section of page */
+    /* since its flex the entire space is filled */
     <main className="flex h-screen font-inter w-full">
       <SideBar user={localUser} />
-      {children}
+      {/* we need to present users with a mobile nav in case of insufficient screenspace
+      on smaller devices as sidebar will be hidden */}
+      <div className="flex size-full flex-col">
+        {/*This class provides styling for the logo and hamburger icon to appear at the ends*/}
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" alt="menu-logo" width={30} height={30} />
+          <MobileNav user={localUser} />
+        </div>
+        {/* Note: Since the layout has the parent flex prop:
+          1. Sidebar and children / Home page will appear next to each other (as expected for sidebar)
+          2. When adding a MobileNav div between, the mobileNav would appear next to the home page child element.
+          3. This would occur when sidebar is hidden due to nested props that control this behaviour
+          4. Now when we remove the children and place it within the parent div that contains the mobileNav+logo
+            the flex col applies thereby rendering the mobileNav at the top
+        */}
+        {children}
+      </div>
     </main>
   );
 }
